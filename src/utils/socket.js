03a562bb3483716ccf8ffc7ffd7e5d532537006e
shinadhmuhammed <muhammedshinad2@gmail.com>
2025-11-14 13:01:20 +0530

    fixes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const baseUrl =
   window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://eramworkforce.com";
+    : "https://eramworkforce.com/";
 
 export const socket = io(baseUrl, {
   withCredentials: true,
